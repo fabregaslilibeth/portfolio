0f9b702db11665d4ea868f6e092b6f3c2b31e11b
Beth <lilibeth.fabregas@coreproc.ph>
2020-09-20 03:51:22 +0800

    add project responsiveness

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,42 +1,39 @@
 <template>
   <div class="container mx-auto">
     <div class="position-fixed" style="left: 10px; top: 10px; z-index: 99;" @click="back"><i class="fas fa-arrow-left mr-2"></i>Back</div>
-<!--    <div class="project-banner-container mx-auto">-->
-<!--      <p><img :src="project.banner" alt="" class="project-banner"></p>-->
-<!--    </div>-->
     <div class="project-details position-relative col-12 mx-auto p-4 bg-white" data-animaton="animated slideUp">
       <h3>{{ project.name }}</h3>
       <h5>{{ project.description }}</h5>
       <div>
-        <small>MADE WITH:</small>
         <small v-for="icon in icons" :key="icon">
-          <i :class="icon.icon" class="mx-2" :style="`color: ${icon.color}`"></i>{{ icon.name }}
+          <img :src="icon.image" alt="" class="icon">
+          {{ icon.name }}
         </small>
       </div>
 
-      <div class="btn-group col-4 m-4" role="group" >
+      <div class="btn-group col-md-4 m-md-4 " role="group">
         <a :href="project.code" target="_blank" class="btn btn-outline-secondary">
-          <i class="fas fa-code mr-2" title="view code"></i><small>View Code</small>
+          <i class="fas fa-code mr-2" title="view code"></i><small class="d-none d-md-inline-block">View Code</small>
         </a>
         <a :href="project.live" target="_blank" class="btn btn-outline-secondary">
-          <i class="fas fa-globe-europe mr-2" title="visit site"></i><small>Visit Site</small>
+          <i class="fas fa-globe-europe mr-2" title="visit site"></i><small class="d-none d-md-inline-block">Visit Site</small>
         </a>
       </div>
 
-      <p class="position-absolute disclaimer"><i class="fas fa-info-circle mr-2 text-secondary" title="All images used in this site are not mine."></i></p>
+      <p class="position-absolute disclaimer d-none d-md-inline-block"><i class="fas fa-info-circle mr-2 text-secondary" title="All images used in this site are not mine."></i></p>
 
     </div>
 
-    <div class="divider px-4 py-2" :style="`background: linear-gradient(to right, ${project.color}, transparent)`">
-      <h3 class="text-left text-white">Demo</h3>
+    <div class="divider px-4 py-2">
+      <h3 class="text-left header"  :style="`color: ${project.color}`">DEMO</h3>
     </div>
 
     <div class="demo px-sm-1 py-4 px-md-4" v-for="video in project.demo" :key="video">
       <iframe :src="`${video}&rel=0`" allowfullscreen></iframe>
     </div>
 
-    <div class="divider px-4 py-2" :style="`background: linear-gradient(to right, ${project.color}, transparent)`">
-      <h3 class="text-left text-white">Pages & Features</h3>
+    <div class="divider px-4 py-2">
+      <h3 class="text-left header"  :style="`color: ${project.color}`">Pages & Features</h3>
     </div>
     <div class="features">
       <ol class="p-4">
@@ -44,13 +41,13 @@
               <hr :style="`background: ${project.color}50`">
            <span v-if="index%2" class="row align-items-center">
               <li class="col-12 col-md-6 my-2">{{ feature.desc }}</li>
-              <p class="col-12 col-md-6 my-2"><img :src="feature.image" class="w-75 image" alt=""
+              <p class="col-12 col-md-6 my-2 feature-image"><img :src="feature.image" class="image" alt=""
                                                    @click.prevent="showImage(feature.image, feature.desc)"></p>
            </span>
-           <span v-else class="row align-items-center">
-             <p class="col-12 col-md-6 my-2"><img :src="feature.image" class="w-75 image" alt=""
+           <span v-else class="row align-items-center even-projects">
+             <p class="col-12 col-md-6 my-2 feature-image"><img :src="feature.image" class="image" alt=""
                                                   @click.prevent="showImage(feature.image, feature.desc)"></p>
-             <li class="col-12 col-md-6 my-2">{{ feature.desc }}</li>
+             <li class="col-12 col-md-6 my-2 feature-desc">{{ feature.desc }}</li>
            </span>
 
           </span>
@@ -71,7 +68,7 @@
 <script>
 import Modal from "../components/Modal";
 import projects from "../projects";
-import icons from "../icons";
+import skills from "../skills";
 
 export default {
   name: 'Project',
@@ -88,7 +85,7 @@ export default {
     this.project = projects.find(project => {
       this.icons = []
       project.categories.forEach(category => {
-        this.icons.push(icons.find(icon => icon.name === category))
+        this.icons.push(skills.find(icon => icon.name === category))
       })
       return project.slug === this.$route.params.slug
     })
@@ -112,6 +109,11 @@ export default {
   height: 50px;
 }
 
+.icon {
+  width: 30px;
+  margin: 10px;
+}
+
 .project-banner {
   width: 100%;
   height: auto;
@@ -124,8 +126,13 @@ export default {
   box-shadow: 0 3px 15px rgba(0, 0, 0, 0.2);
 }
 
+.feature-image {
+  cursor: zoom-in;
+}
+
 .image {
   box-shadow: 0 3px 15px rgba(0, 0, 0, 0.2);
+  width: 75%;
 }
 
 .disclaimer {
@@ -137,6 +144,7 @@ export default {
   width: 560px;
   height: 315px;
 }
+
 
 ol {
   list-style: none;
@@ -167,6 +175,7 @@ ol span:nth-child(2n) hr::after {
   right: -40px !important;
 }
 
+
 @media (max-width: 768px) {
   .demo iframe {
     width: 350px;
@@ -178,6 +187,21 @@ ol span:nth-child(2n) hr::after {
   .demo iframe {
     width: 280px;
     height: 164px;
+  }
+
+  .even-projects .feature-image {
+    order: 2;
+    display: flex;
+  }
+
+  .even-projects .feature-desc {
+    order: 1;
+    display: flex;
+  }
+
+  .image {
+    box-shadow: 0 3px 15px rgba(0, 0, 0, 0.2);
+    width: 100%;
   }
 }
 </style>
